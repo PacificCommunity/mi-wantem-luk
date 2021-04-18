dfe2e32dc3441860fbd2d063ff9cba84b45154cf
robscott3 <robert.dryden.scott@gmail.com>
2021-04-18 12:27:25 +1100

    turning random colours off

--- R/chord.r
+++ R/chord.r
@@ -11,6 +11,8 @@
 #'
 #' @param obj:    An object either of class MFCLRegion or of class MFCLTag
 #' @param frq:    An object of MFCLFrq. Defaults to NULL but required if obj=MFCLTag
+#' @param age:    By default the first age is plotted
+#' @param season: By default the first season is plotted
 #'
 #'
 #' @return A plot of movement between assessment regions as determined either by the tag data or the estimated movement from the assessment.
@@ -26,18 +28,19 @@ setGeneric('chordMovePlot', function(obj, ...) standardGeneric('chordMovePlot'))
 
 
 setMethod("chordMovePlot", signature(obj="MFCLRegion"), 
-          function(obj, ...){
+          function(obj, age=1, season=1, ...){
 
             par <- obj
             args <- list(...)
             
-            m       <- diff_coffs_age_period(par)[,,1,1]
+            m       <- diff_coffs_age_period(par)[,,age,season]
             regions <- paste("Region", 1:dim(m)[1])
             
             
             dimnames(m) <- list(from=regions, to=regions)
             #groupColors <- c("#000000", "#FFDD89", "#957244", "#F26223","#000000", "#FFDD89", "#957244", "#F26223")
             rnd <- sample(1:dim(diff_coffs_age_period(par))[1], dim(diff_coffs_age_period(par))[1], replace=F)
+            rnd <-        1:dim(diff_coffs_age_period(par))[1]
             groupColors <- colorRampPalette(c("goldenrod","brown","beige"))(dim(diff_coffs_age_period(par))[1])[rnd]
             #groupColors <- viridis(dim(diff_coffs_age_period(par))[1], option="D")
             
