e1bf1a31e720276f37f116cc8733ff04342a637e
robscott3 <robersc@spc.int>
2023-05-29 13:55:18 +1100

    adding condagelength plots

--- R/assessmentDiagnostics.r
+++ R/assessmentDiagnostics.r
@@ -40,7 +40,7 @@ getEffortDevs <- function(par, frq){
 
 
 ## length frequency plot function
-lenCompFits <- function(lenfit, par=NULL, tpo=NULL, fsh=NULL, years=NULL, ...){
+lenCompFits <- function(lenfit, par=NULL, tpo=NULL, fsh=NULL, years=NULL, name=NULL, ...){
   #browser()
   if(is.null(years)){
     years <- range(lenfit)['minyear']:range(lenfit)['maxyear']  
@@ -87,14 +87,14 @@ lenCompFits <- function(lenfit, par=NULL, tpo=NULL, fsh=NULL, years=NULL, ...){
            else 
              panel.xyplot(...)
            
-           if(!is.null(tpo)){
-             panel.text(max(lenfitsub$length)*0.9, max(lenfitsub$obs)*0.95, round(sampsize[panel.number()],2), cex=0.8)
-             panel.text(max(lenfitsub$length)*0.9, max(lenfitsub$obs)*0.80, round(llvals[panel.number()],2), cex=0.8)
-           }
            if(!is.null(par)){
              panel.abline(v=laa(par, ages=seq(1,dimensions(par)['agecls'], by=4)), col="lightgrey")
+           }
+           if(!is.null(tpo)){
+             panel.text(max(lenfitsub$length)*0.9, max(lenfitsub$obs)*0.95, paste("N =", round(sampsize[panel.number()],0)), cex=0.8)
+             panel.text(max(lenfitsub$length)*0.9, max(lenfitsub$obs)*0.80, paste("-LL =", round(llvals[panel.number()],0)), cex=0.8)
            }},
-         key=list(space='top',columns=2, lines=list(lty=1, col=c("black","magenta")), text=list(c("observed","predicted"))), ...)
+         key=list(space='top',columns=2, lines=list(lty=c(1,1), col=c("black","magenta")), text=list(c("observed","predicted"))), ...)
 }
 
 #pp  <- read.MFCLPar('/home/rob/MSE/ofp-sam-albacore_MSE/OMs/CC_grid/CC_JH/06.par')
@@ -103,12 +103,101 @@ lenCompFits <- function(lenfit, par=NULL, tpo=NULL, fsh=NULL, years=NULL, ...){
 
 #pp  <- read.MFCLPar('/home/rob/MSE/ofp-sam-albacore_MSE/OMs/CC_grid/run17/06.par')
 #tpo <- read.MFCLLikelihood('/home/rob/MSE/ofp-sam-albacore_MSE/OMs/CC_grid/run17/test_plot_output')
-#lfit<- read.MFCLLenFit2('/home/rob/MSE/ofp-sam-albacore_MSE/OMs/CC_grid/run17/length.fit')
+#lfit<- read.MFCLLenFit2('/home/rob/MSE/ofp-sam-albacore_MSE/OMs/CC_grid/run29/length.fit', get_lenage=T)
 
-#lenCompFits(lfit, pp, tpo, fsh=15, years=2010:2014)
+#lenCompFits(trim(lfit, length=0:100), pp, tpo, fsh=15, years=2010:2014)
 
 
 
+condLenAgeFits <- function(lfit, alk=NULL, ...){
+  #browser()
+  #merge the lenfit data and the ALK
+  ddlfit <- cbind(subset(lenagefits(lfit), pred>0), dtype='pred')
+  colnames(ddlfit)[6] <- 'data'
+  ddalk  <- cbind(subset(ALK(alk), obs>0), dtype='obs')
+  colnames(ddalk)[7] <- 'data'
+  # rescale the ALK to proportions - consistent with lenfit
+  tempdatsum <- sum(ddalk$data)  #tapply(ddalk$data, list(ddalk$year, ddalk$month, ddalk$fishery), sum)
+  tempdatn   <- nrow(ddalk)      #tapply(ddalk$data, list(ddalk$year, ddalk$month, ddalk$fishery), length)
+  ddalk$data <- ddalk$data/tempdatsum
+  
+  dd     <<- rbind(ddlfit[,c('year','month','fishery','age','length','data','dtype')],
+                  ddalk[,c('year','month','fishery','age','length','data','dtype')])
+  
+  ll <- sort(unique(dd$length))
+  aa <- seq(range(unique(dd$age))[1], range(unique(dd$age))[2], length=length(ll))
+  
+  par(mfrow=c(length(unique(dd$year)), length(unique(dd$month))), mar=c(0,0,0,0), omi=c(0.8,0.8,0.5,0.5))
+  
+  for(yy in sort(unique(dd$year))){
+    for(mm in sort(unique(dd$month))){
+      if(mm==min(sort(unique(dd$month))) & yy<max(sort(unique(dd$year))))  # graphics settings for left column plots
+        plot(ll,aa, type='n', xaxt='n', xlab='', ylab='')
+      if(mm>min(sort(unique(dd$month))) & yy==max(sort(unique(dd$year))))  # graphics settings for bottom row plots
+        plot(ll,aa, type='n', yaxt='n', xlab='', ylab='')
+      if(mm==min(sort(unique(dd$month))) & yy==max(sort(unique(dd$year))))  # graphics settings for bottom left corner
+        plot(ll,aa, type='n', xlab='', ylab='')
+      if(mm>min(sort(unique(dd$month))) & yy<max(sort(unique(dd$year))))  # graphics settings for all others
+        plot(ll,aa, type='n', xaxt='n', yaxt='n', xlab='', ylab='')
+      
+      text(max(ll)*0.25, max(aa)*0.9, paste('Year =', yy))
+      text(max(ll)*0.25, max(aa)*0.75, paste('Month =', mm))
+        if(nrow(subset(dd, year==yy & month==mm & dtype=='pred'))>0)  
+          symbols(subset(dd, year==yy & month==mm & dtype=='pred')$length, subset(dd, year==yy & month==mm & dtype=='pred')$age, 
+                circles=subset(dd, year==yy & month==mm & dtype=='pred')$data, inches=0.1, fg='cyan', add=T)
+        if(nrow(subset(dd, year==yy & month==mm & dtype=='obs'))>0)
+          symbols(subset(dd, year==yy & month==mm & dtype=='obs')$length, subset(dd, year==yy & month==mm & dtype=='obs')$age, 
+                circles=subset(dd, year==yy & month==mm & dtype=='obs')$data, inches=0.1, fg='blue', add=T)
+        lines(c(laa(lfit)), c(as.numeric(dimnames(laa(lfit))$age))/4)
+      }
+  }
+  #mtext('Length', side=1)
+  #mtext('Age',    side=2) 
+  
+}
+
+
+#condLenAgeFits(trim(lfityft, fishery=16, year=2010:2019), trim(alkyft, fishery=16, year=2010:2019))
+
+
+
+
+
+
+## Not really working - switching to a non-lattice approach
+
+#condLenAgeFitsX <- function(lfit, alk=NULL, ...){
+  #browser()
+  #merge the lenfit data and the ALK
+#  ddlfit <- cbind(subset(lenagefits(lfit), pred>0), dtype='pred', col='red')
+#  colnames(ddlfit)[6] <- 'data'
+#  ddalk  <- cbind(subset(ALK(alk), obs>0), dtype='obs', col='blue')
+#  colnames(ddalk)[7] <- 'data'
+  # rescale the ALK to proportions - consistent with lenfit
+#  tempdatsum <- tapply(ddalk$data, list(ddalk$year, ddalk$month, ddalk$fishery), sum)
+#  tempdatn   <- tapply(ddalk$data, list(ddalk$year, ddalk$month, ddalk$fishery), length)
+#  ddalk$data <- ddalk$data/rep(tempdatsum, tempdatn)/10
+  
+#  dd     <- rbind(ddlfit[,c('year','month','fishery','age','length','data','dtype','col')],
+#                  ddalk[,c('year','month','fishery','age','length','data','dtype','col')])
+  
+#  cc <- lapply(unique(dd$month), function(x){subset(dd, month==x)$data})
+  
+#  pxfun <- function(x,y, pancex=pancex, ...){      
+    #browser()
+#    panel.xyplot(x,y, cex=pancex[[panel.number()]], ...)
+#    panel.lines(c(laa(lfit)), as.numeric(dimnames(laa(lfit))$age)/4, col="black")
+#  }
+  
+#  xyplot(age~length|as.factor(month)*as.character(year), group=dtype, data=dd, pch=1, col=c('red','blue'), 
+#         pancex=list(c(4,1),c(c(8,2,3,4,5,6,7,8),2),c(1,4),c(2,5)),
+#         panel=pxfun)
+#         }
+
+
+#lfityft <- read.MFCLLenFit2('/media/sf_assessments/yft/2023/model_runs/stepwise/03_PreCatchCond/03j_No_Effort_Projections/length.fit', get_lenage=T)
+#alkyft  <- read.MFCLALK('/media/sf_assessments/yft/2023/model_runs/stepwise/03_PreCatchCond/03j_No_Effort_Projections/yft.age_length', lenfit=lfityft)
+#condLenAgeFits(trim(lfityft, fishery=16, year=2010), trim(alkyft, fishery=16, year=2010))
 
 
 
