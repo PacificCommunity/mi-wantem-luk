02272bad25413aba70c8d4ee472398710ffa7a0d
robscott3 <robersc@spc.int>
2023-06-06 10:33:06 +1100

    removing plot for the time being

--- R/plot.r
+++ R/plot.r
@@ -19,44 +19,44 @@
 #'
 #' @aliases mfcl
 
-setMethod("plot", signature(x="MFCLLenFit"), function(x, y="missing", ...){
-  #browser()
-  args <- list(...)
-  
-  if(any(is.element(lapply(args, class), "MFCLLikelihood"))){
-    ppx <- lenCompFits(x, y, ...)
-    plot(ppx)
-  }
-  if(any(is.element(lapply(args, class), "MFCLALK"))){
-    ppx <- condLenAgeFits(x, y, ...)
-    plot(ppx)
-  }
-})
+#setMethod("plot", signature(x="MFCLLenFit"), function(x, y="missing", ...){
+#  #browser()
+#  args <- list(...)
+#  
+#  if(any(is.element(lapply(args, class), "MFCLLikelihood"))){
+#    ppx <- lenCompFits(x, y, ...)
+#    plot(ppx)
+#  }
+#  if(any(is.element(lapply(args, class), "MFCLALK"))){
+#    ppx <- condLenAgeFits(x, y, ...)
+#    plot(ppx)
+#  }
+#})
 
+#testing
 #plot(trim(lfit, length=0:100), pp, tpo, fsh=15, years=2010:2014)
-
 #plot(trim(lfityft, fishery=16, year=2010:2019), trim(alkyft, fishery=16, year=2010:2019))
 
-setMethod("plot", signature(x="MFCLRegion"), function(x, y="missing", ...){
-  #browser()
-  args <- list(...)
- 
-  if(args$type=="Sankey"){
-    sankeyMovePlot(x, ...)
-  }
-  if(args$type=="Chord"){
-    chordMovePlot(x, ...)
-  }
-})
+#setMethod("plot", signature(x="MFCLRegion"), function(x, y="missing", ...){
+#  #browser()
+#  args <- list(...)
+# 
+#  if(args$type=="Sankey"){
+#    sankeyMovePlot(x, ...)
+#  }
+#  if(args$type=="Chord"){
+#    chordMovePlot(x, ...)
+#  }
+#})
   
 
 
-setMethod("plot", signature(x="MFCLTag"), function(x, y="missing", ...){
-  #browser()
-  args <- list(...)
+#setMethod("plot", signature(x="MFCLTag"), function(x, y="missing", ...){
+#  #browser()
+#  args <- list(...)
   
-  chordMovePlot(x, ...)
+#  chordMovePlot(x, ...)
   
-})
+#})
 
 
